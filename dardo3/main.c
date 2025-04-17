9fb6404bff9b4f7f1dc8876fc6ee41f0dcaa37d3
WagnerSFranca123 <wagnerdesouzafranca@gmail.com>
2025-04-17 10:38:47 -0300

    Exercicio 'Dardo' em C

---
+++ dardo3/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*No arremesso de dardo, o atleta tem três chances para lançar o dardo à maior distância que conseguir.
+Você deve criar um programa para, dadas as medidas das três tentativas de lançamento, informar qual
+foi a maior.
+*/
+
+int main()
+{
+    float dist1, dist2, dist3;
+
+    printf("Digite as tres distancias:\n");
+    scanf("%f%f%f", &dist1, &dist2, &dist3);
+
+    if (dist1>dist2&&dist1>dist3)
+    {
+        printf("Maior distancia: %.2f", dist1);
+    }
+    else if (dist2>dist3)
+    {
+        printf("Maior distancia: %.2f", dist2);
+    }
+    else
+    {
+        printf("Maior distancia: %.2f", dist3);
+    }
+    return 0;
+}
